5261bb38a3e20a52db28dcff1b26f9bb547f30b8
zhangjianyun <zhangjianyun_kd@163.com>
2020-09-28 22:20:16 +0800

    ribbon全局配置，正确实现方式

--- src/main/java/com/lengedyun/contentcenter/config/UserRibbonConfig.java
+++ src/main/java/com/lengedyun/contentcenter/config/UserRibbonConfig.java
@@ -2,6 +2,7 @@ package com.lengedyun.contentcenter.config;
 
 import com.lengedyun.ribbonconfig.RibbonConfig;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * @date: 2020/9/28 21:21
  */
 //@Configuration
-//@RibbonClient(name = "user-center",configuration = RibbonConfig.class)
+//@RibbonClient(name = "user-center",configuration = RibbonConfig.class)//单个服务配置
+//@RibbonClients(defaultConfiguration=RibbonConfig.class)//全局配置
 public class UserRibbonConfig {
 }
