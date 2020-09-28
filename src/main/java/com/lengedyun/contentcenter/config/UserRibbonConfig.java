1db443f3c9a2e1ef27eac11095dc8b86af194430
zhangjianyun <zhangjianyun_kd@163.com>
2020-09-28 22:10:25 +0800

    配置文件代码自定义ribbon策略，建议使用配置简单没有坑

--- src/main/java/com/lengedyun/contentcenter/config/UserRibbonConfig.java
+++ src/main/java/com/lengedyun/contentcenter/config/UserRibbonConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
  * @auther: 张健云
  * @date: 2020/9/28 21:21
  */
-@Configuration
-@RibbonClient(name = "user-center",configuration = RibbonConfig.class)
+//@Configuration
+//@RibbonClient(name = "user-center",configuration = RibbonConfig.class)
 public class UserRibbonConfig {
 }
