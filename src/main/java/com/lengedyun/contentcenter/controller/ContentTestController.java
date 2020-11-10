b4f96f3e17ba994b991e54b0eec96d3335d53c1e
zhangjianyun <zhangjianyun_kd@163.com>
2020-11-10 08:36:10 +0800

    restTemplate exchange方式实现toekn传递

--- src/main/java/com/lengedyun/contentcenter/controller/ContentTestController.java
+++ src/main/java/com/lengedyun/contentcenter/controller/ContentTestController.java
@@ -5,9 +5,17 @@ import com.lengedyun.contentcenter.feign.TestBaiduFeignClient;
 import com.lengedyun.contentcenter.feign.TestUserCenterFeignClient;
 import com.lengedyun.contentcenter.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @title: TestController
@@ -28,6 +36,9 @@ public class ContentTestController {
     @Autowired
     TestService testService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("getReqMuiltArgs")
     public UserDto query(UserDto userDto){
         return userCenterFeignClient.getReqMuiltArgs(userDto);
@@ -46,5 +57,22 @@ public class ContentTestController {
     @GetMapping("test-b")
     public String testB(){
         return testService.common()+" test-b";
+    }
+
+    /*
+    * restTemplate exchange方式实现toekn传递
+    * */
+    @GetMapping("tokenRelay/{userId}")
+    public ResponseEntity<UserDto> query(@PathVariable Integer userId, HttpServletRequest request){
+        String token = request.getHeader("X-token");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-token",token);
+        return this.restTemplate.exchange(
+            "http://user-center/users/{userId}",
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                UserDto.class,
+                userId
+        );
     }
 }
