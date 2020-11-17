c6b0a584544cdbfff4666994b38dff0b6224e1cf
zhangjianyun <zhangjianyun_kd@163.com>
2020-11-18 07:02:53 +0800

    通过配置中心管理配置

--- src/main/java/com/lengedyun/contentcenter/controller/ContentTestController.java
+++ src/main/java/com/lengedyun/contentcenter/controller/ContentTestController.java
@@ -5,6 +5,7 @@ import com.lengedyun.contentcenter.feign.TestBaiduFeignClient;
 import com.lengedyun.contentcenter.feign.TestUserCenterFeignClient;
 import com.lengedyun.contentcenter.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -40,35 +41,35 @@ public class ContentTestController {
     RestTemplate restTemplate;
 
     @GetMapping("getReqMuiltArgs")
-    public UserDto query(UserDto userDto){
+    public UserDto query(UserDto userDto) {
         return userCenterFeignClient.getReqMuiltArgs(userDto);
     }
 
     @GetMapping("reqBaidu")
-    public String index(){
-       return testBaiduFeignClient.index();
+    public String index() {
+        return testBaiduFeignClient.index();
     }
 
     @GetMapping("test-a")
-    public String testA(){
-        return testService.common()+" test-a";
+    public String testA() {
+        return testService.common() + " test-a";
     }
 
     @GetMapping("test-b")
-    public String testB(){
-        return testService.common()+" test-b";
+    public String testB() {
+        return testService.common() + " test-b";
     }
 
     /*
-    * restTemplate exchange方式实现toekn传递
-    * */
+     * restTemplate exchange方式实现toekn传递
+     * */
     @GetMapping("tokenRelay/{userId}")
-    public ResponseEntity<UserDto> query(@PathVariable Integer userId, HttpServletRequest request){
+    public ResponseEntity<UserDto> query(@PathVariable Integer userId, HttpServletRequest request) {
         String token = request.getHeader("X-token");
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-token",token);
+        headers.add("X-token", token);
         return this.restTemplate.exchange(
-            "http://user-center/users/{userId}",
+                "http://user-center/users/{userId}",
                 HttpMethod.GET,
                 new HttpEntity<>(headers),
                 UserDto.class,
@@ -80,11 +81,20 @@ public class ContentTestController {
      * restTemplate interceptor方式实现toekn传递
      * */
     @GetMapping("tokenRelayInterceptor/{userId}")
-    public ResponseEntity<UserDto> tokenRelayInterceptor(@PathVariable Integer userId){
+    public ResponseEntity<UserDto> tokenRelayInterceptor(@PathVariable Integer userId) {
         return this.restTemplate.getForEntity(
                 "http://user-center/users/{userId}",
                 UserDto.class,
                 userId
         );
+    }
+
+    @Value("${your.config}")
+    private String yourConfig;
+
+    @GetMapping("getNacosConfig")
+    public String nacosConfig() {
+
+        return yourConfig;
     }
 }
